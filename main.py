8155e979cd01d2308aafaa6473c5940b7a4c3545
tinytim1985 <137758146+tinytim1985@users.noreply.github.com>
2023-06-26 02:06:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randrange
 
 # TODO if using Github diff deployment on HeroKu uncomment the next line
-#import os
+import os
 import discord
 from discord.ext import commands
 
@@ -209,7 +209,7 @@ async def on_guild_channel_pins_update(channel, last_pin):
 
 
 # TODO Replace TOKEN with the token from discord developer portal 
-client.run('MTAwOTEyNjIzMTU4Njg5ODAyMg.GOPYKu.B3uxCiiU9IEOp3mWgoPe5Rq6b6w5Ynptc25Ews')
+#client.run('MTEyMjc2OTcyMzU1ODQ3Mzc5OQ.Gpkjoq.JBf4tf5Uj19_mVgkrkLCnz3kZn3C_KtvNl8GxA')
 
 # TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
-#client.run(os.environ.get('TOKEN'))
+client.run(os.environ.get('TOKEN'))
